381e40ba9956abbe819356f23ef34d78e766d4ec
BVanBuren <bvanburenwx@gmail.com>
2021-11-05 17:44:21 -0400

    exported default the reducers and it is being called in the app.js. removed the useReducer that is imported from react because it is no longer needed

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,3 @@
-import { useReducer } from "react";
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -95,6 +94,4 @@ export const reducer = (state = initialState, action) => {
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState)
-}
+export default reducers;
