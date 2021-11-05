7deeea7af0394b33f9758dbe252a1326997bc0d3
BVanBuren <bvanburenwx@gmail.com>
2021-11-05 17:11:41 -0400

    imported useSelector and useDispatch from react-redux, removed usecontext. added appropriate comments to the code

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -7,12 +7,16 @@ import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+// import the useSelector and the useDispatch from react-redux
+import { useSelector, useDispatch } from 'react-redux';
 import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  // remove the useStoreContext and add useSelector and useDispatch
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
