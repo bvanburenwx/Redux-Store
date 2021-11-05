f5cbc4a07a7586377d4215191398d6873afc3d51
BVanBuren <bvanburenwx@gmail.com>
2021-11-05 17:31:50 -0400

    Went back through component index files and deleted the remaining useStoreContext and made comments explaining added coded

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+// import useSelector and useDispatch from react-redux
 import { useSelector, useDispatch } from 'react-redux';
 
 function ProductItem(item) {
+  // refactor code to remove useStoreContext and instead use useSelector and useDispatch
   const state = useSelector(state => state);
   const dispatch = useDispatch();
 
